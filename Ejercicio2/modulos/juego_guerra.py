86e49550321c7d16f2360c09b272786ca72cb1ee
Maxalba36 <maxalba@gmail.com>
2022-09-27 01:08:08 -0300

    probando mostrar cartas

--- Ejercicio2/modulos/juego_guerra.py
+++ Ejercicio2/modulos/juego_guerra.py
@@ -14,13 +14,12 @@ boca_arriba = False
 boca_abajo = False
 
 
-#clase CARTA
+#----------- Clase CARTA ------------
 class Carta:
     
     def __init__ (self,val,pal):
         self.valor = val
         self.palo = pal
-        
         
     def mostrar_carta(self):
       print(self.valor + self.palo)
@@ -32,7 +31,9 @@ class Carta:
     def is_boca_abajo(self):
         boca_abajo = True
         print('X')
-#----------------------------------------
+        
+        
+#----------- Clase MAZO ------------
 class Mazo:
     def __init__(self):
         self.cartas = ListaDobleEnlazada()
@@ -46,86 +47,89 @@ class Mazo:
         random.shuffle(cartas_ordenadas)
         for carta in cartas_ordenadas:
             self.cartas.anexar(carta)
+            
 
-class JuegoGuerra:
+    def Mostrar_Mazo(self):
         
-        def __init__ (self,semilla):
-            random.seed(semilla)
-            self.jugador1 = ListaDobleEnlazada()
-            self.jugador2 = ListaDobleEnlazada()
+        print(self.cartas)
+
+# class JuegoGuerra:
+        
+#         def __init__ (self,semilla):
+#             random.seed(semilla)
+#             self.jugador1 = ListaDobleEnlazada()
+#             self.jugador2 = ListaDobleEnlazada()
             
-        def masos(self):
-            maso = [self.genera_carta()] 
-            i=1
-            while (i<52):
-                nueva_carta = (self.genera_carta())
-                if nueva_carta not in maso: 
-                    maso.append(nueva_carta)
-                    i+=1
-            print(maso)
+#         def masos(self):
+#             maso = [self.genera_carta()] 
+#             i=1
+#             while (i<52):
+#                 nueva_carta = (self.genera_carta())
+#                 if nueva_carta not in maso: 
+#                     maso.append(nueva_carta)
+#                     i+=1
+#             print(maso)
             
-            i=0
-            while(i<52):
-                self.jugador1.anexar(maso[i])
-                self.jugador2.anexar(maso[i+1])
-                i+=2
+#             i=0
+#             while(i<52):
+#                 self.jugador1.anexar(maso[i])
+#                 self.jugador2.anexar(maso[i+1])
+#                 i+=2
         
-        def genera_carta(self):
-            carta = []
-            carta.append(valores[random.randint(0, 12)])
-            carta.append(palos[random.randint(0, 3)])
-            return carta
+#         def genera_carta(self):
+#             carta = []
+#             carta.append(valores[random.randint(0, 12)])
+#             carta.append(palos[random.randint(0, 3)])
+#             return carta
         
-        def jugar(self):
-            turno = 0
-            en_juego = []
-            while (self.jugador1.tamanio()!=0 or self.jugador2.tamanio()!=0):
-            #while (turno<2):
-                en_juego.append(self.jugador1.extraer())
-                en_juego.append(self.jugador2.extraer())
+#         def jugar(self):
+#             turno = 0
+#             en_juego = []
+#             while (self.jugador1.tamanio()!=0 or self.jugador2.tamanio()!=0):
+#             #while (turno<2):
+#                 en_juego.append(self.jugador1.extraer())
+#                 en_juego.append(self.jugador2.extraer())
                 
-                if en_juego[len(en_juego)-2][0]=='A' and en_juego[len(en_juego)-1][0]=='A' or en_juego[len(en_juego)-2][0]==en_juego[len(en_juego)-1][0]:
-                    print("Guerra!")
-                    turno+=1
-                    continue
+#                 if en_juego[len(en_juego)-2][0]=='A' and en_juego[len(en_juego)-1][0]=='A' or en_juego[len(en_juego)-2][0]==en_juego[len(en_juego)-1][0]:
+#                     print("Guerra!")
+#                     turno+=1
+#                     continue
 
-                print("-------------------------------------------")
-                print(f'Turno: {turno}')
-                print("Jugador 1:")
-                for j in range(1, self.jugador1.tamanio()+1):
-                    if j%10 == 0 :
-                        fin_de_linea = '\n'
-                    else:
-                        fin_de_linea = ""
-                    print("-X ",end=fin_de_linea)
+#                 print("-------------------------------------------")
+#                 print(f'Turno: {turno}')
+#                 print("Jugador 1:")
+#                 for j in range(1, self.jugador1.tamanio()+1):
+#                     if j%10 == 0 :
+#                         fin_de_linea = '\n'
+#                     else:
+#                         fin_de_linea = ""
+#                     print("-X ",end=fin_de_linea)
                     
-                print("")
-                print("            ",end="")
-                print(en_juego[len(en_juego)-2][0] + en_juego[len(en_juego)-2][1] + " ",end="")
-                print(en_juego[len(en_juego)-1][0] + en_juego[len(en_juego)-1][1])
-                print("")
+#                 print("")
+#                 print("            ",end="")
+#                 print(en_juego[len(en_juego)-2][0] + en_juego[len(en_juego)-2][1] + " ",end="")
+#                 print(en_juego[len(en_juego)-1][0] + en_juego[len(en_juego)-1][1])
+#                 print("")
                 
-                if en_juego[len(en_juego)-2][0]>en_juego[len(en_juego)-1][0]:
-                        self.jugador1.agregar(en_juego.pop())
-                        self.jugador1.agregar(en_juego.pop())
-                print("Jugador 2:")
-                for j in range(1, self.jugador2.tamanio()+1):
-                    if j%10 == 0 :
-                        fin_de_linea = '\n'
-                    else:
-                        fin_de_linea = ""
-                    print("-X ",end=fin_de_linea)
-                print("")
-                turno+=1
+#                 if en_juego[len(en_juego)-2][0]>en_juego[len(en_juego)-1][0]:
+#                         self.jugador1.agregar(en_juego.pop())
+#                         self.jugador1.agregar(en_juego.pop())
+#                 print("Jugador 2:")
+#                 for j in range(1, self.jugador2.tamanio()+1):
+#                     if j%10 == 0 :
+#                         fin_de_linea = '\n'
+#                     else:
+#                         fin_de_linea = ""
+#                     print("-X ",end=fin_de_linea)
+#                 print("")
+#                 turno+=1
  
-#PRUEBAS
-if __name__ == "__main__":
+# #PRUEBAS
+# if __name__ == "__main__":
     
- car = Carta('2','♥')       
- #car.mostrar_carta()    
- #car.is_boca_arriba()    
- car.is_boca_abajo() 
-                
-                
-                
+  # car = Carta('2','♥')       
+  # #car.mostrar_carta()    
+  # #car.is_boca_arriba()    
+  # car.is_boca_abajo()            
+                            
         
